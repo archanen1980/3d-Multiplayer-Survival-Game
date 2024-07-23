f7fb92943b531dd4c4f95d5d406d76a881dc864e
archanen1980 <archanen@gmail.com>
2024-07-23 10:25:33 -0400

    Fixed an issue where if items were in a container that got unequiped they would be deleted instead of moved to another container

--- Assets/HughesJeremiahAssets/InventorySystem/Scripts/EquipmentSlot.cs
+++ Assets/HughesJeremiahAssets/InventorySystem/Scripts/EquipmentSlot.cs
@@ -85,12 +85,23 @@ public class EquipmentSlot : MonoBehaviour, IPointerClickHandler
                 }
             }
 
-            // Now clear any remaining excess slots
+            // Move remaining items in excess slots to other containers
             foreach (var slot in excessSlots)
             {
                 if (slot.GetItem() != null)
                 {
-                    slot.ClearSlot();
+                    InventoryItem item = slot.GetItem();
+                    int itemCount = slot.GetItemCount();
+                    int remainingItems = MoveItemToAvailableContainers(item, itemCount);
+
+                    if (remainingItems > 0)
+                    {
+                        slot.AddItem(item, remainingItems); // Keep remaining items in the slot if they couldn't be moved
+                    }
+                    else
+                    {
+                        slot.ClearSlot(); // Clear the slot if all items were moved
+                    }
                 }
             }
 
@@ -101,43 +112,6 @@ public class EquipmentSlot : MonoBehaviour, IPointerClickHandler
         currentItem = null;
         icon.sprite = null;
         icon.enabled = false;
-    }
-
-
-
-    public bool CanEquipItem(EquipmentItem item)
-    {
-        return item.itemType == slotType;
-    }
-
-    private void MoveItemsFromContainer(ItemContainer container)
-    {
-        List<InventorySlot> slotsToClear = new List<InventorySlot>();
-
-        // Move items within the same container first
-        MoveItemsWithinContainer(container);
-
-        foreach (var slot in container.slots)
-        {
-            if (slot.GetItem() != null)
-            {
-                int itemCount = slot.GetItemCount();
-                InventoryItem item = slot.GetItem();
-                int remainingItems = MoveItemToAvailableContainers(item, itemCount);
-
-                if (remainingItems > 0)
-                {
-                    Debug.Log($"Deleted {remainingItems} of {item.itemName} because no available slots.");
-                }
-
-                slotsToClear.Add(slot);
-            }
-        }
-
-        foreach (var slot in slotsToClear)
-        {
-            slot.ClearSlot();
-        }
     }
 
     private int MoveItemToAvailableContainers(InventoryItem item, int count)
@@ -160,111 +134,9 @@ public class EquipmentSlot : MonoBehaviour, IPointerClickHandler
         return remainingCount; // Return remaining items if not all could be moved
     }
 
-    private bool CanMoveItemsFromContainer(ItemContainer container)
+    public bool CanEquipItem(EquipmentItem item)
     {
-        foreach (var slot in container.slots)
-        {
-            if (slot.GetItem() != null)
-            {
-                int itemCount = slot.GetItemCount();
-                InventoryItem item = slot.GetItem();
-                int remainingCount = CheckAvailableSpaceForItem(item, itemCount);
-
-                if (remainingCount > 0)
-                {
-                    return false; // Not enough space available
-                }
-            }
-        }
-
-        return true; // Enough space is available
-    }
-
-    private int CheckAvailableSpaceForItem(InventoryItem item, int count)
-    {
-        int remainingCount = count;
-
-        foreach (var container in InventoryManager.instance.containers)
-        {
-            if (container != associatedContainer) // Avoid checking space in the associated container
-            {
-                foreach (var slot in container.slots)
-                {
-                    if (slot.GetItem() == null || (slot.GetItem().itemID == item.itemID && item.isStackable))
-                    {
-                        int spaceLeft = item.maxStackSize - slot.GetItemCount();
-                        if (spaceLeft > 0)
-                        {
-                            int itemsToAdd = Mathf.Min(spaceLeft, remainingCount);
-                            remainingCount -= itemsToAdd;
-
-                            if (remainingCount <= 0)
-                            {
-                                return 0; // Enough space found
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return remainingCount; // Return remaining items if not all could be moved
-    }
-
-    private bool WillHaveSpaceInContainer(ItemContainer container)
-    {
-        int availableSlots = container.minSlotCount;
-
-        foreach (var slot in container.slots)
-        {
-            if (slot.GetItem() == null)
-            {
-                availableSlots--;
-            }
-            else if (slot.GetItem().isStackable)
-            {
-                int spaceLeft = slot.GetItem().maxStackSize - slot.GetItemCount();
-                if (spaceLeft > 0)
-                {
-                    availableSlots--;
-                }
-            }
-
-            if (availableSlots <= 0)
-            {
-                return true; // Enough space available in the container
-            }
-        }
-
-        return false; // Not enough space available in the container
-    }
-
-    private void MoveItemsWithinContainer(ItemContainer container)
-    {
-        List<InventorySlot> slotsToClear = new List<InventorySlot>();
-
-        // Move items from additional slots to minimum slots within the same container
-        for (int i = container.minSlotCount; i < container.slots.Count; i++)
-        {
-            InventorySlot slot = container.slots[i];
-            if (slot.GetItem() != null)
-            {
-                int itemCount = slot.GetItemCount();
-                InventoryItem item = slot.GetItem();
-
-                int remainingItems = MoveItemToMinSlotsWithinContainer(container, item, itemCount);
-
-                if (remainingItems > 0)
-                {
-                    slotsToClear.Add(slot); // Mark slot to be cleared later
-                }
-            }
-        }
-
-        foreach (var slot in slotsToClear)
-        {
-            slot.ClearSlot();
-        }
+        return item.itemType == slotType;
     }
 
     private int MoveItemToMinSlotsWithinContainer(ItemContainer container, InventoryItem item, int count)
